f38f569724adfc730b7f24ebbe0e85d424fbea93
Michael Zidar <mzidar@gmail.com>
2021-07-19 22:14:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+let satFun, monFun, flair, flair2
+
+
+function saturdayFun(satFun){
+    if (typeof(satFun) === "undefined"){
+        return 'This Saturday, I want to roller-skate!' 
+    }else{
+        return 'This Saturday, I want to ' +satFun + '!' 
+    }
+     
+}
+
+const mondayWork = function(monFun){
+    if(typeof(monFun) === "undefined")
+    {
+        return 'This Monday, I will go to the office.'
+    }
+    else{
+        return 'This Monday, I will ' +monFun + '.'
+    }
+}
+
+function wrapAdjective(flair = '*'){
+    return function(flair2){
+        return 'You are ' +flair +flair2 +flair + '!'
+    }
+}
